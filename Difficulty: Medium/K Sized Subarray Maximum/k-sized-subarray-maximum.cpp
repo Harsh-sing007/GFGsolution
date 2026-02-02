90458ac749b87bc69b6352be7bbc95bd9b54884a
Harsh singh <149600919+Harsh-sing007@users.noreply.github.com>
2026-02-02 22:31:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,15 +1,23 @@
 class Solution {
-  public:
+public:
     vector<int> maxOfSubarrays(vector<int>& arr, int k) {
         deque<int> dq;
         vector<int> ans;
 
         for (int i = 0; i < arr.size(); i++) {
-            if (!dq.empty() && dq.front() == i - k)
+
+            // Remove elements out of this window
+            if (!dq.empty() && dq.front() <= i - k)
                 dq.pop_front();
+
+            // Remove smaller elements (they are useless)
             while (!dq.empty() && arr[dq.back()] <= arr[i])
                 dq.pop_back();
+
+            // Add current index
             dq.push_back(i);
+
+            // Window formed
             if (i >= k - 1)
                 ans.push_back(arr[dq.front()]);
         }
