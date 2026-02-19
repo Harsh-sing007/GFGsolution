ad90305488255e19844c6df5afcd738bdc52f81c
Harsh singh <149600919+Harsh-sing007@users.noreply.github.com>
2026-02-19 23:23:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+  
+    void dfsHelper(int node, vector<vector<int>>& adj,
+                   vector<int>& visited, vector<int>& result) {
+        
+        visited[node] = 1;
+        result.push_back(node);
+        
+        for(int neighbor : adj[node]) {
+            if(!visited[neighbor]) {
+                dfsHelper(neighbor, adj, visited, result);
+            }
+        }
+    }
+  
+    vector<int> dfs(vector<vector<int>>& adj) {
+        int V = adj.size();
+        vector<int> visited(V, 0);
+        vector<int> result;
+        
+        dfsHelper(0, adj, visited, result);
+        
+        return result;
+    }
+};
