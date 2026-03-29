56a9e526940ab7bddeb9765824ace4cdbf24a677
Harsh singh <149600919+Harsh-sing007@users.noreply.github.com>
2026-03-29 16:03:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countPartitions(vector<int>& arr, int diff) {
+        int n = arr.size();
+        
+        int totalSum = 0;
+        for(int x : arr) totalSum += x;
+        if ((totalSum + diff) % 2 != 0) return 0;
+        
+        int target = (totalSum + diff) / 2;
+        
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        
+        for(int num : arr) {
+            for(int j = target; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        
+        return dp[target];
+    }
+};
